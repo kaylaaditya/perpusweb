a5ce2d48db77b2c7a97e542d962d7bcf74cded66
kaylaaditya <kaylaaditya123@outlook.com>
2023-12-13 07:09:43 +0700

    memisah elemen2 dashboard

---
+++ resources/views/login/register.blade.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Registrasi</title>
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
+    <link rel="stylesheet" href="/adminlte/plugins/fontawesome-free/css/all.min.css">
+
+    <link rel="stylesheet" href="/adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+
+    <link rel="stylesheet" href="/adminlte/dist/css/adminlte.min.css?v=3.2.0">
+    <script nonce="19fdfcbf-cf78-4fa7-990c-e9719f9799b6">
+        (function(w, d) {
+            ! function(j, k, l, m) {
+                j[l] = j[l] || {};
+                j[l].executed = [];
+                j.zaraz = {
+                    deferred: [],
+                    listeners: []
+                };
+                j.zaraz.q = [];
+                j.zaraz._f = function(n) {
+                    return async function() {
+                        var o = Array.prototype.slice.call(arguments);
+                        j.zaraz.q.push({
+                            m: n,
+                            a: o
+                        })
+                    }
+                };
+                for (const p of ["track", "set", "debug"]) j.zaraz[p] = j.zaraz._f(p);
+                j.zaraz.init = () => {
+                    var q = k.getElementsByTagName(m)[0],
+                        r = k.createElement(m),
+                        s = k.getElementsByTagName("title")[0];
+                    s && (j[l].t = k.getElementsByTagName("title")[0].text);
+                    j[l].x = Math.random();
+                    j[l].w = j.screen.width;
+                    j[l].h = j.screen.height;
+                    j[l].j = j.innerHeight;
+                    j[l].e = j.innerWidth;
+                    j[l].l = j.location.href;
+                    j[l].r = k.referrer;
+                    j[l].k = j.screen.colorDepth;
+                    j[l].n = k.characterSet;
+                    j[l].o = (new Date).getTimezoneOffset();
+                    if (j.dataLayer)
+                        for (const w of Object.entries(Object.entries(dataLayer).reduce(((x, y) => ({
+                                ...x[1],
+                                ...y[1]
+                            })), {}))) zaraz.set(w[0], w[1], {
+                            scope: "page"
+                        });
+                    j[l].q = [];
+                    for (; j.zaraz.q.length;) {
+                        const z = j.zaraz.q.shift();
+                        j[l].q.push(z)
+                    }
+                    r.defer = !0;
+                    for (const A of [localStorage, sessionStorage]) Object.keys(A || {}).filter((C => C.startsWith("_zaraz_"))).forEach((B => {
+                        try {
+                            j[l]["z_" + B.slice(7)] = JSON.parse(A.getItem(B))
+                        } catch {
+                            j[l]["z_" + B.slice(7)] = A.getItem(B)
+                        }
+                    }));
+                    r.referrerPolicy = "origin";
+                    r.src = "/cdn-cgi/zaraz/s.js?z=" + btoa(encodeURIComponent(JSON.stringify(j[l])));
+                    q.parentNode.insertBefore(r, q)
+                };
+                ["complete", "interactive"].includes(k.readyState) ? zaraz.init() : j.addEventListener("DOMContentLoaded", zaraz.init)
+            }(w, d, "zarazData", "script");
+        })(window, document);
+    </script>
+</head>
+
+<body class="hold-transition login-page">
+    <div style="width: 50vw" class="login-box">
+        <div class="login-logo">
+            <a href="../../index2.html"><b>Registrasi</b>PERPUSWEB</a>
+        </div>
+
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="row justify-content-center">
+                            <div class="col-4">
+                                <img src="http://localhost:8000/logoperpus.jpeg" style="width: 100px"/>
+                            </div>
+                        </div>
+                        <div class="text-center mt-2">
+                            <h2>SISTEM INFORMASI PERPUSWEB</h2>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="text-center">
+                            Halaman Registrasi
+                        </div>
+                        <div>
+                            <form method="POST" action="">
+                                @csrf
+                                <div class="form-group mt-4">
+                                    <label for="email" class="text-md-right">{{ __('Username') }}</label>
+                                    <div class="">
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                        @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="form-group mt-4">
+                                    <label for="email" class="text-md-right">{{ __('Email') }}</label>
+                                    <div class="">
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                        @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="password" class="text-md-right">{{ __('Password') }}</label>
+                                    <div class="">
+                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                        @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="mb-0">
+                                    <button type="submit" class="btn btn-primary btn-block">
+                                        Register
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="/adminlte/plugins/jquery/jquery.min.js"></script>
+
+    <script src="/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script src="/adminlte/dist/js/adminlte.min.js?v=3.2.0"></script>
+</body>
+
+</html>
